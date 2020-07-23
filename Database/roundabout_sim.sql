c5844d12006e3605902406541cbdaba55d59f7db
frisco087 <53129657+frisco087@users.noreply.github.com>
2020-07-23 16:16:55 +0100

    Add files via upload

--- Database/roundabout_sim.sql
+++ Database/roundabout_sim.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 01, 2020 at 06:41 PM
+-- Generation Time: Jul 23, 2020 at 05:15 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
@@ -33,19 +33,18 @@ CREATE TABLE `user` (
   `name` varchar(20) NOT NULL,
   `password` varchar(40) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `bio` varchar(300) DEFAULT NULL
+  `bio` varchar(300) DEFAULT NULL,
+  `Score` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `password`, `email`, `bio`) VALUES
-(17, 'admin', '202cb962ac59075b964b07152d234b70', 'frisco@live.ie', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cill'),
-(21, 'AAhlert1', 'b89a6f93bd5469df2937c1d17927f2fc', 'anneahlert@hotmail.com', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cill'),
-(23, 'test', '202cb962ac59075b964b07152d234b70', 'frisco@live.ie', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cill'),
-(38, 'a', 'c2f0789e6ad28c3f6f85da1fb9828d79', 'a@a.com', 'sdsds'),
-(44, 'd', '8277e0910d750195b448797616e091ad', 'a@a.com', NULL);
+INSERT INTO `user` (`id`, `name`, `password`, `email`, `bio`, `Score`) VALUES
+(17, 'admin', '202cb962ac59075b964b07152d234b70', 'frisco@live.ie', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cill', NULL),
+(21, 'AAhlert1', 'b89a6f93bd5469df2937c1d17927f2fc', 'anneahlert@hotmail.com', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cill', NULL),
+(75, 'daniel', '8277e0910d750195b448797616e091ad', 'frisco@live.ie', 'my bio d', '2');
 
 --
 -- Indexes for dumped tables
@@ -65,7 +64,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
